4ca681e4bc5f3bc9a9821bf427ede32e8e52fa56
Trungnguyen1011 <nttorange96@gmail.com>
2021-05-20 23:14:09 +0700

    them bai tap

--- Java-core/hw/b2/Formula.java
+++ Java-core/hw/b2/Formula.java
@@ -1,80 +1,160 @@
-import java.util.Arrays;
 
 public class Formula {
-    public float average (int[] array) {
+    public float average(int[] array) {
         float sum = 0f;
         for (int a = 0; a < array.length; a++) {
             sum += array[a];
         }
-        return sum/array.length;
+        return sum / array.length;
     }
 
-    public int[] reverseArray (int[] array) {
-        int s = 0; 
-        int e = array.length - 1 ;
+    public int[] reverseArray(int[] array) {
+        int s = 0;
+        int e = array.length - 1;
         int n;
         while (s < e) {
-        n = array[s];
-        array[s] = array[e];
-        array[e] = n;
-        s++; e--;
-     }
+            n = array[s];
+            array[s] = array[e];
+            array[e] = n;
+            s++;
+            e--;
+        }
         return array;
     }
 
-    public static void listed (int[] array) {
+    public void listed(int[] array) {
         int i;
-        for (i = 0 ; i < array.length; i++)
-        {
+        for (i = 0; i < array.length; i++) {
             System.out.print(array[i] + ", ");
         }
 
     }
 
-    public int countOddNumber (int[] array) {
-        int o = 0; int i;
-        for (i = 0 ; i < array.length ; i++) 
-        {
-            if (array[i] % 2 == 1) { o++;}
+    public int countOddNumber(int[] array) {
+        int o = 0;
+        int i;
+        for (i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                o++;
+            }
 
         }
         return o;
     }
 
-
-    public static void listedButOddOnly (int[] array) {
+    public void listedButOddOnly(int[] array) {
         int i;
-        for (i = 0 ; i < array.length; i++)
-        { 
+        for (i = 0; i < array.length; i++) {
             if (array[i] % 2 == 1) {
-            System.out.print(array[i] + ", "); }
-        }
-        
-
-    }
-
-    public void bmiRating(float height, float weight) { 
-        float bmi = (weight/(height*height));
-        if (bmi >= 25.0f) { 
-            if (bmi >= 30.0f ) { 
-                System.out.println( "Rất nguy hiểm");
-            } else { 
-                System.out.println( "quá cân");
+                System.out.print(array[i] + ", ");
             }
         }
-        else { if (bmi >= 18.5 ) {
-            System.out.println( "bình thường");
-           } else { 
-            System.out.println( "nhẹ cân");
-           }
-        
-        
-        }
-        
-        
-        
+
+    }
+
+    public void bmiRating(float height, float weight) {
+        float bmi = (weight / (height * height));
+        if (bmi >= 25.0f) {
+            if (bmi >= 30.0f) {
+                System.out.println("Rất nguy hiểm");
+            } else {
+                System.out.println("quá cân");
+            }
+        } else {
+            if (bmi >= 18.5) {
+                System.out.println("bình thường");
+            } else {
+                System.out.println("nhẹ cân");
+            }
+
         }
 
     }
 
+    public void triangle(int N) {
 
+        if (N < 10 && N > 3) { // Chọn số N theo điều kiện
+            int i;
+            int u;
+            /* Vòng lặp tổng sẽ xuống với số dòng tương ứng với giá trị của N */
+            for (i = 1; i <= N; i++) {
+                System.out.println("");
+                /*
+                 * Vòng lặp cấp 2 tác dụng trong một dòng, sẽ chạy hết rồi mới quay lại vòng lặp
+                 * tổng, sẽ vẽ "*" tương ứng với giá trị của i, i= 1 vẽ 1 *, i = 2 vẽ 2*,...
+                 */
+                for (u = i; u > 0; u--) {
+                    System.out.print("* ");
+                }
+            }
+        }
+    }
+
+    public void hollowTriangle(int N) {
+
+        if (N < 10 && N > 3) { // Chọn số N theo điều kiện
+            int i;
+            int u;
+            /* Vòng lặp tổng sẽ xuống với số dòng tương ứng với giá trị của N */
+            for (i = 1; i <= N; i++) {
+                System.out.println("");
+                /*
+                 * Giả sử N = m(số dòng cạnh trên) + n(số dòng cạnh dưới), ưu tiên cạnh trên tam
+                 * giác dài hơn cạnh dưới. m, n nguyên dương.
+                 */
+                // N chẵn: m = N/2 + 1, n = N/2 -1.
+                // N lẻ: m = n + 1. m = N/2 + 0.5.
+                /*
+                 * điều kiện 1: nếu số dòng (giá trị i) đã đến điểm giữa hay chưa, do N có thể
+                 * chẵn, ưu tiên cạnh trên tam giác dài hơn cạnh dưới nên cho thêm N/2 + 1 (m).
+                 * Bất kể N chẵn hay lẻ thì i sẽ vượt qua điểm giữa khi i = m + 1.
+                 */
+                if (i <= (N / 2 + 1)) {
+                    for (u = i; u > 0; u--) { /* vòng lặp cấp 2 trong điều kiện số dòng chưa vượt qua điểm giữa */
+                        /*
+                         * vẽ "*" theo giá trị của i như câu 6, nhưng chỉ tối đa là 2 "*" ở đầu và cuối
+                         * dòng, nên đặt điều kiện chỉ vẽ * khi u có giá trị đầu tiên (u = i) và giá trị
+                         * cuối cùng (u = 1) bởi u = 0 thì không vẽ "*"
+                         */
+                        if (u == i || u == 1) {
+                            System.out.print("* ");
+                        } else {
+                            System.out.print(
+                                    "  "); /* nếu không phải giá trị đầu cuối thì thêm khoảng trắng vào giữa "  " */
+                        }
+                    }
+                } else { /* vòng lặp cấp 2 trong điều kiện số dòng đã qua điểm giữa */
+
+                    if (N % 2 == 1) { /* điều kiện 2 nếu N là số lẻ */
+                        for (u = (N + 1) - i; u > 0; u--) {
+                            /*
+                             * Vẫn tiếp tục vẽ "*" ở giá trị đầu và cuối đổi u = (N+1) - i trong đó "- i"
+                             * bởi lúc này i càng tăng thì số khoảng trắng càng giảm, u = (N + 1) vì lúc này
+                             * i = m + 1 . (N + 1) - i <=> (m + n + 1 ) - m -1 = n, số dòng cạnh dưới.
+                             */
+                            if (u == (N + 1) - i || u == 1) {
+                                System.out.print("* ");
+                            } else {
+                                System.out.print("  ");
+                            }
+                        }
+
+                    } else { /* điều kiện 2 nếu N là số chẵn */
+                        for (u = (N + 1) - i; u > 0; u--) {
+
+                            if (u == (N + 1) - i || u == 1) {
+                                System.out.print("*  ");
+                            } else {
+                                System.out.print("   "); // tăng thêm vài lần " " cho cân đối :v
+                            }
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+
+}
--- Java-core/hw/b2/HomeWork.java
+++ Java-core/hw/b2/HomeWork.java
@@ -1,39 +1,35 @@
 
-
 public class HomeWork {
     public static void main(String[] args) {
 
-
         System.out.println("Bài tập về nhà");
         Formula formula = new Formula();
-        int[] array = new int[] {10, 20, 30 ,40 ,50};
+        int[] array = new int[] { 10, 20, 30, 40, 50 };
         System.out.println("Câu sô 1: Tính trung bình tổng các số trong mảng: ");
         System.out.print("[ ");
         formula.listed(array);
         System.out.println("] ");
         System.out.println("Bài toán có kết quả là: " + formula.average(array));
 
-        
-
-        int[] reverseMe = new int[] {1, 2, 3 ,4 ,5};
+        int[] reverseMe = new int[] { 1, 2, 3, 4, 5 };
         System.out.println("Câu số 2: Đảo ngược thứ tự mảng sau: ");
         System.out.print("[ ");
         formula.listed(reverseMe);
         System.out.println("] ");
-        System.out.println("Có kết quả là: "); 
+        System.out.println("Có kết quả là: ");
         formula.reverseArray(reverseMe);
         System.out.print("[ ");
         formula.listed(reverseMe);
         System.out.println("] ");
-        
-        int[] howManyOddinME = new int[] {1, 2, 3, 4, 5, 6, 7};
+
+        int[] howManyOddinME = new int[] { 1, 2, 3, 4, 5, 6, 7 };
         System.out.println("Câu số 3: Số lượng số lẻ trong mảng này: ");
         System.out.print("[ ");
         formula.listed(howManyOddinME);
         System.out.println("] ");
         System.out.println("là: " + formula.countOddNumber(howManyOddinME));
 
-        int[] sortOutTheOdd = new int[] {1, 2, 3, 4, 5, 6, 7};
+        int[] sortOutTheOdd = new int[] { 1, 2, 3, 4, 5, 6, 7 };
         System.out.println("Câu số 4: Những sô lẻ của mảng này: ");
         formula.listed(sortOutTheOdd);
         System.out.println("là: ");
@@ -44,13 +40,19 @@ public class HomeWork {
         float height = 1.74f;
         float weight = 80.0f;
         System.out.println("Câu số 5: Đánh giá thể trạng qua chỉ số BMI: ");
-        System.out.println("Thể trạng của người có cân nặng " + weight + "kg" 
-        + " và chiều cao là " + height + "m được cho là "
-        ); 
+        System.out.println(
+                "Thể trạng của người có cân nặng " + weight + "kg" + " và chiều cao là " + height + "m được cho là ");
         formula.bmiRating(height, weight);
 
+        int N = 8;
+        System.out.println("Câu số 6: Vẽ hình tam giác sao với chiều cao bằng số nguyên dương N = " + N);
+        formula.triangle(N);
+        System.out.println("");
+
+        int M = 8;
+        System.out.println("Câu số 7: Vẽ hình tam giác rỗng số nguyên dương N = " + M);
+        formula.hollowTriangle(M);
 
     }
 
 }
-

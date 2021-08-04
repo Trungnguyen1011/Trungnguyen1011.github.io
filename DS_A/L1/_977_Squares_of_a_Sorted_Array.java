cad4e7e1b9cbb4501655f942a9c34b05882ed451
Trungnguyen1011 <82566556+Trungnguyen1011@users.noreply.github.com>
2021-08-04 17:42:12 +0700

    hw 1

--- DS_A/L1/_977_Squares_of_a_Sorted_Array.java
+++ DS_A/L1/_977_Squares_of_a_Sorted_Array.java
@@ -29,28 +29,6 @@ public class _977_Squares_of_a_Sorted_Array {
 
         return newArr;
     }
-    public static int[] sortedSquaresss(int[] nums) {
-        if (nums.length == 0) { return nums;}
-        int[] newArray = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] *= nums[i];
-        }
-        int j = nums.length -1; int max = 0; int indexToRemove = 0;
-        while (j >=0) {
-            max =0;
-        for (int k = 0; k < nums.length; k++) {
-            if (nums[k] > max) {
-                max =nums[k];
-                indexToRemove = k ;
-            }
-
-        }
-        newArray[j] = max;
-        nums[indexToRemove] =0;
-        j--;
-    }
-        
-        return newArray;
-      }
+   
     }
 
